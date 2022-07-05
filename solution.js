93fe76ae24d42e254a0636ae2460828c21f7aee5
dci-student <alinacuznetov@gmail.com>
2022-07-05 12:48:51 +0200

    math

---
+++ solution.js
@@ -0,0 +1,25 @@
+/* 1 */
+
+log.console(min(-1, ..., 4));
+log.console(max(-1, ..., 4));
+
+
+/* 2 */
+
+log.console(math.ceil(3321.32321));
+log.console(math.ceil(326.76));
+log.console(math.ceil(76788.7));
+log.console(math.ceil(-9.78));
+log.console(math.ceil(43.342));
+
+log.console(math.floor(3321.32321));
+log.console(math.floor(326.76));
+log.console(math.floor(76788.7));
+log.console(math.floor(-9.78));
+log.console(math.floor(43.342));
+
+
+
+/* 3 */
+
+log.console(math.random(1, ..., 6));
