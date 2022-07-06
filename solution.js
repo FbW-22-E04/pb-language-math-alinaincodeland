a186074c045100460f3fb578dfb34a3f6f266d02
dci-student <alinacuznetov@gmail.com>
2022-07-06 15:58:13 +0200

    error solved

--- solution.js
+++ solution.js
@@ -1,25 +1,25 @@
 /* 1 */
 
-log.console(min(-1, ..., 4));
-log.console(max(-1, ..., 4));
+console.log(min(-1, ..., 4));
+console.log(max(-1, ..., 4));
 
 
 /* 2 */
 
-log.console(math.ceil(3321.32321));
-log.console(math.ceil(326.76));
-log.console(math.ceil(76788.7));
-log.console(math.ceil(-9.78));
-log.console(math.ceil(43.342));
+console.log(math.ceil(3321.32321));
+console.log(math.ceil(326.76));
+console.log(math.ceil(76788.7));
+console.log(math.ceil(-9.78));
+console.log(math.ceil(43.342));
 
-log.console(math.floor(3321.32321));
-log.console(math.floor(326.76));
-log.console(math.floor(76788.7));
-log.console(math.floor(-9.78));
-log.console(math.floor(43.342));
+console.log(math.floor(3321.32321));
+console.log(math.floor(326.76));
+console.log(math.floor(76788.7));
+console.log(math.floor(-9.78));
+console.log(math.floor(43.342));
 
 
 
 /* 3 */
 
-log.console(math.random(1, ..., 6));
+console.log(math.random(1, ..., 6));
